1f17201fc22f44be2906f8e9d384873ae51c21a5
Victor Matheus <victormatheus507@gmail.com>
2024-08-02 16:53:50 -0300

    app router

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ContatoPage() {
+  return (
+    <main>
+      <h2>Contato</h2>
+    </main>
+  );
+}
---
+++ src/components/menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Menu() {
+  return (
+    <ul className="menu">
+      <li>Home</li>
+      <li>Sobre</li>
+      <li>Contato</li>
+    </ul>
+  );
+}
