b6c5b90506409870713c4729482db831b35ad187
Victor Matheus <victormatheus507@gmail.com>
2024-08-02 17:18:17 -0300

    Server e Client Components

---
+++ src/components/acesso.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import fs from 'fs/promises';
+
+export default async function Acesso() {
+  await fs.appendFile('acesso.txt', `${Date.now()}`, 'utf8');
+  const data = await fs.readFile('acesso.txt', 'utf8');
+  
+  return <div>{data}</div>;
+}
---
+++ src/components/width.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+
+export default function Width() {
+  const [width, setWidth] = useState(10);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(document.documentElement.clientWidth);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const [ativo, setAtivo] = useState(false);
+
+  return (
+    <div>
+      <h2 style={{ color: ativo ? '#680' : '#b00' }}>
+        Largura da tela: {width}
+      </h2>
+      <button onClick={() => setAtivo((b) => !b)}>Ativar</button>
+    </div>
+  );
+}
