c9f24631af50e9787e320a2cd062b231a4d571b1
Victor Matheus <victormatheus507@gmail.com>
2024-08-02 17:00:05 -0300

    Link

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Menu() {
   return (
     <ul className="menu">
-      <li>Home</li>
-      <li>Sobre</li>
-      <li>Contato</li>
+      <li>
+        <Link href={'/'}> Home</Link>
+      </li>
+      <li>
+        <Link href={'/sobre'}> Sobre</Link>
+      </li>
+      <li>
+        <Link href={'/contato'}> Contato</Link>
+      </li>
     </ul>
   );
 }
