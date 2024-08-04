3ca3ad249dc8c7e4dd9c2e022a92420213aad14a
Victor Matheus <victormatheus507@gmail.com>
2024-08-04 20:10:49 -0300

    server actions 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+'use client';
+import setCookie from '@/actions/set-cookie';
 import Acesso from '@/components/acesso';
 import ServerFetch from '@/components/serverFetch';
 
-export default async function HomePage() {
+export default function HomePage() {
+  function handleClick() {
+    console.log('teste');
+    setCookie('segredo', '123456');
+  }
+
   return (
     <main>
       <h1>Home</h1>
-      <Acesso />
-      <ServerFetch />
+      {/* <Acesso /> */}
+      {/* <ServerFetch /> */}
+
+      <button onClick={handleClick}>Definit Cookie</button>
     </main>
   );
 }
