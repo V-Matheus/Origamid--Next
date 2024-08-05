8ef7daf7bc052bcde31749d29a32e525de23ba8a
Victor Matheus <victormatheus507@gmail.com>
2024-08-04 22:09:11 -0300

    erro

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,32 +1,11 @@
+import ProdutosLista from '@/components/produtos-lista';
 import React from 'react';
 
-export type Produto = {
-  id?: string;
-  nome: string;
-  preco: number;
-  descricao: string;
-  estoque: number;
-  importado: 0 | 1;
-};
-
 export default async function ProdutosPage() {
-  const resposne = await fetch('https://api.origamid.online/produtos', {
-    next: {
-      revalidate: 5,
-    },
-  });
-  const produtos = (await resposne.json()) as Produto[];
-
   return (
     <main>
       <h1>Produtos</h1>
-      <ul>
-        {produtos.map((produto) => (
-          <li key={produto.id}>
-            {produto.nome}: R$ {produto.preco}
-          </li>
-        ))}
-      </ul>
+      <ProdutosLista />
     </main>
   );
 }
